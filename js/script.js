3ebf6103058ac0ba8d4e09989eee93e4d31b4e55
giuseppemaisto <gmaisto98@gmail.com>
2022-12-13 16:46:37 +0100

    MILESTONE 0

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//1 - Definisco array team
+//2 - inserisco oggetti persone in array
+//2.1 - definisco in oggetti nome, ruolo e foto 
+
+
+//MILESTONE 0
+const Team =[
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder &  CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'Angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Develoer',
+        foto: 'scott-estrada-developer.jpg',
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg',
+    },
+]
