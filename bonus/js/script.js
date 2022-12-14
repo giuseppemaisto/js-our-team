7f06d441b540c0d887e0106915611034c34c56ca
giuseppemaisto <gmaisto98@gmail.com>
2022-12-14 10:24:50 +0100

    BONUS 2 COMPLETATO

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -40,12 +40,12 @@ const Team =[
 //MILESTONE 1
 //3 - stampo su console per ogni oggetto, tutte le informazioni
   //  3.1 - utilizzo ciclo for
-  let stampaTeam 
-  for(let i = 0; i<Team.length;i++){
-    stampaTeam = Team[i];
+ // let stampaTeam 
+  //for(let i = 0; i<Team.length;i++){
+   // stampaTeam = Team[i];
     //console.log(stampaTeam);
-    console.log(stampaTeam.nome , stampaTeam.ruolo, stampaTeam.foto);
-  }
+   // console.log(stampaTeam.nome , stampaTeam.ruolo, stampaTeam.foto);
+ // }
 
   //MILESTONE 2
   
@@ -55,11 +55,37 @@ const Team =[
   //4.3 - considerare immagine come una stringa 
 
   //BONUS-1
- const divContent = document.getElementById('team')
+ //const divContent = document.getElementById('team')
+ //for(let i = 0; i<Team.length; i++){
+  //  divContent.innerHTML +=   
+  //  `  <div class="team">${Team[i].nome}, 
+   // ${Team[i].ruolo}, 
+   // <img src ="./img/${Team[i].foto}" alt="">
+  //  </div>`;
+// }
+
+//BONUS-2
+
+function drawTeamMember(person){
+    const divContent = document.getElementById('cards-container')
+
+    let member_card = ` 
+    <div class ="team-card">
+        <div class= "card-image">
+
+        <img class = "img-fluid" src ="./img/${person.foto}" </div> 
+        <div class="card-text">
+        <h4>${person.nome}</h4>
+        <p>${person.ruolo}</p>
+        </div>
+        </div>
+        ` 
+    divContent.innerHTML += member_card;
+    } 
+
+
+
  for(let i = 0; i<Team.length; i++){
-    divContent.innerHTML +=   
-    `  <div class="team">${Team[i].nome}, 
-    ${Team[i].ruolo}, 
-    <img src ="./img/${Team[i].foto}" alt="">
-    </div>`;
+    let member = Team[i];
+    drawTeamMember(member);
  }
